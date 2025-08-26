f42e6071b8df1eb063467001513327b968faa892
Bilal Ahmar <bilalahmar245@gmail.com>
2025-08-26 12:37:15 +0500

    UI upgrade

--- src/utilities/Tooltip.jsx
+++ src/utilities/Tooltip.jsx
@@ -4,9 +4,11 @@ export default function Tooltip({ children, title }) {
     return (
         <div className="relative group">
             {children}
-            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-sm rounded px-2 py-1">
-                {title}
-            </div>
+            {title && (
+                <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-sm rounded px-2 py-1">
+                    {title}
+                </div>
+            )}
         </div>
     );
 }
